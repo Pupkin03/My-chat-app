1ac4c948bfe18518e98d408c34a4c0869a58298e
Abejide Ayomide <Abejideyoungzee@gmail.com>
2024-06-06 13:26:28 +0100

    main

---
+++ client/src/Routes.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { useContext } from "react";
+import { UserContext } from "./UserContext";
+import RegisterAndLoginForm from "./RegisterAndLoginForm";
+import Chat from "./Chat";
+
+export default function Routes() {
+    const {username, id} = useContext(UserContext);
+
+    if (username) {
+        return <Chat/>
+    }
+
+
+    return (
+        <RegisterAndLoginForm/>
+    );
+
+}
